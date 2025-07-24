fa0a7fa0d6b55d6c888b6127a6b6687fd2c04c36
Hamza Almughrabi <hmughrabi2001@gmail.com>
2025-07-24 19:51:41 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const navList = document.getElementById('navbar__list');
+const allSections = document.querySelectorAll('section');
+
+allSections.forEach((sec, i) => {
+  const li = document.createElement('li');
+  const link = document.createElement('a');
+
+  let secName = "Section " + (i + 1);
+  let secId = sec.id;
+
+  link.textContent = secName;
+  link.href = `#${secId}`;
+  link.className = 'menu__link';
+
+  li.appendChild(link);
+  navList.appendChild(li);
+});
